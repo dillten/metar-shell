a3bccc3ec4c92d0d4f8ad650c10f961ab3ae316d
Dillon TenBrink <dillon@tenbrink.us>
2016-08-22 13:56:05 -0500

    Adding some help text.

--- metar.sh
+++ metar.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#!/bin/bash
+if (( $# < 2 )); then
+    printf "This command requires two inputs. A comma separated list of stations and the number of hours to retrieve."
+    exit 1
+fi
 SITE=$1
 HOURS=$2
 curl -s https://aviationweather.gov/adds/dataserver_current/httpparam\?dataSource\=metars\&requestType\=retrieve\&format\=xml\&stationString\=$SITE\&hoursBeforeNow\=$HOURS\&fields\=raw_text | grep raw_text | sed -e 's,.*<raw_text>\([^<]*\)</raw_text>.*,\1,g'
